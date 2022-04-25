bc6d4b50e2e7e0c6c544349c003ab43847d63849
ayselelibol <elibolaysel@gmail.com>
2022-04-25 12:49:31 +0300

    aysel düzenliyor

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print("ege1")
+    print("aysel")
 
 
 # Press the green button in the gutter to run the script.
